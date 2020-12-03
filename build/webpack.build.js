6530ec635590388ff0d0f35e5487e765ca1c8020
millent <millent@126.com>
2020-12-03 18:27:45 +0800

    init code

---
+++ build/webpack.build.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const dir = __dirname
+
+module.exports = {
+  entry: path.resolve(dir, '../src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(dir, '../dist'),
+  },
+};
